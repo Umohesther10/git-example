adb675e1e65f5c266744dc0d6b446638a7742fc8
Umohesther10 <59563888+Umohesther10@users.noreply.github.com>
2020-04-22 13:44:17 +0100

    chnages made to users information

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-
-   "Nazir", "Kemi"
-
+const users = [
+  {
+    name: "Umoh Esther Mercy",
+    age: 22,
+    gender: "Female",
+  },
+  {
+    name: "Abdullahi Ridwan",
+    age: 25,
+    gender: "male",
+  },
+  {
+    name: "Ikihde Bright",
+    age: 28,
+    gender: "male",
+  },
+  {
+    name: "Grace Itiung",
+    age: 23,
+    gender: "Female",
+  },
+];
+console.log(users)
